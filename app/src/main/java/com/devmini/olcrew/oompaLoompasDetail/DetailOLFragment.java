e5f1b750e5baf9b0b9896852e085cd478d8378cb
MerylMP <maria.mphob@gmail.com>
2019-12-25 19:51:00 +0100

    Retrieving OL detail from API

--- app/src/main/java/com/devmini/olcrew/oompaLoompasDetail/DetailOLFragment.java
+++ app/src/main/java/com/devmini/olcrew/oompaLoompasDetail/DetailOLFragment.java
@@ -1,23 +1,29 @@
 package com.devmini.olcrew.oompaLoompasDetail;
 
 
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.devmini.olcrew.R;
+import com.devmini.olcrew.modelData.OompaLoompa;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class DetailOLFragment extends Fragment {
+public class DetailOLFragment extends Fragment implements DetailOLMVPInterface.View {
+
+    private DetailOLMVPInterface.Presenter presenter;
+    private int oompaLoompaId;
+    private OompaLoompa oompaLoompa;
 
     private ImageView imageView;
     private TextView firstName;
@@ -37,6 +43,13 @@ public class DetailOLFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Bundle bundle = getArguments();
+        oompaLoompaId = bundle.getInt("oompaSelected");
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -47,6 +60,8 @@ public class DetailOLFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        presenter = new DetailOLFragmentPresenter(this);
 
         imageView = view.findViewById(R.id.detailsView_image);
         firstName = view.findViewById(R.id.detailsView_firstName);
@@ -60,5 +75,52 @@ public class DetailOLFragment extends Fragment {
         color = view.findViewById(R.id.detailsView_color);
         food = view.findViewById(R.id.detailsView_food);
         description = view.findViewById(R.id.detailsView_description);
+
+        this.presenter.getOompaLoompaById(oompaLoompaId);
+    }
+
+    @Override
+    public void loadOompaLoompa(OompaLoompa oompaLoompa) {
+
+        this.oompaLoompa = oompaLoompa;
+
+        this.firstName.setText(this.oompaLoompa.getFirst_name());
+        this.lastName.setText(this.oompaLoompa.getLast_name());
+        this.profession.setText(this.oompaLoompa.getProfession());
+        this.email.setText(this.oompaLoompa.getEmail());
+        this.age.setText(String.format("%s", this.oompaLoompa.getAge()));
+        this.height.setText(String.format("%s", this.oompaLoompa.getHeight()));
+        this.gender.setText(this.oompaLoompa.getGender());
+        this.country.setText(this.oompaLoompa.getCountry());
+        this.color.setText(this.oompaLoompa.getFavorite().getColor());
+        this.food.setText(this.oompaLoompa.getFavorite().getFood());
+
+        // Formatting description
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            this.description.setText(Html.fromHtml(oompaLoompa.getDescription(), Html.FROM_HTML_MODE_COMPACT));
+        } else {
+            this.description.setText(Html.fromHtml(oompaLoompa.getDescription()));
+        }
+
+        // Setting ImageView
+        if (oompaLoompa.getImage().isEmpty()) {
+            Glide.with(this)
+                    .load(R.drawable.olplaceholder)
+                    .centerCrop()
+                    .placeholder(R.drawable.olplaceholder)
+                    .into(this.imageView);
+        } else {
+            Glide.with(this)
+                    .load(oompaLoompa.getImage())
+                    .centerCrop()
+                    .placeholder(R.drawable.olplaceholder)
+                    .into(this.imageView);
+        }
+    }
+
+    @Override
+    public void showError(String error) {
+        // TODO improve layout
+        Toast.makeText(getContext(), error, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/devmini/olcrew/oompaLoompasDetail/DetailOLFragmentModel.java
+++ app/src/main/java/com/devmini/olcrew/oompaLoompasDetail/DetailOLFragmentModel.java
@@ -1,4 +1,49 @@
 package com.devmini.olcrew.oompaLoompasDetail;
 
+import android.util.Log;
+
+import com.devmini.olcrew.modelData.OompaLoompa;
+import com.devmini.olcrew.retrofit.OompaLoompasAPI;
+import com.devmini.olcrew.retrofit.OompaLoompasClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class DetailOLFragmentModel implements DetailOLMVPInterface.Model {
+
+    private DetailOLMVPInterface.Presenter presenter;
+    private OompaLoompasAPI oompaLoompasAPI;
+
+
+    public DetailOLFragmentModel(DetailOLMVPInterface.Presenter presenter) {
+        this.presenter = presenter;
+        this.oompaLoompasAPI = OompaLoompasClient.getAPI();
+    }
+
+
+    @Override
+    public void getOompaLoompasById(int oompaID) {
+
+        Call<OompaLoompa> call = oompaLoompasAPI.getOompaLoompaById(oompaID);
+        call.enqueue(new Callback<OompaLoompa>() {
+            @Override
+            public void onResponse(Call<OompaLoompa> call, Response<OompaLoompa> response) {
+                if (!response.isSuccessful()) {
+                    Log.d("OompaLoompa_details", "Unsuccesful response. Code: " + response.code());
+                    presenter.onFailureResponse("Error al recuperar la información");
+                    return;
+                }
+
+                OompaLoompa oompaLoompa = response.body();
+                presenter.retrievedOLById(oompaLoompa);
+            }
+
+            @Override
+            public void onFailure(Call<OompaLoompa> call, Throwable t) {
+                Log.d("OompaLoompa_details", "Response failed. " + t.getMessage());
+                presenter.onFailureResponse("Error en la conexión con el servidor");
+            }
+        });
+    }
 }

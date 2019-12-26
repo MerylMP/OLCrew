38913d832cfe0769c5e83cbdd0dc28755215ce38
MerylMP <maria.mphob@gmail.com>
2019-12-26 19:45:17 +0100

    implementing Toolbar

--- app/src/main/java/com/devmini/olcrew/activities/MainActivity.java
+++ app/src/main/java/com/devmini/olcrew/activities/MainActivity.java
@@ -1,8 +1,11 @@
 package com.devmini.olcrew.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -15,10 +18,22 @@ public class MainActivity extends AppCompatActivity {
     private ListOLFragment listOLFragment = null;
     private DetailOLFragment detailOLFragment = null;
 
+    private Toolbar toolbar;
+    private TextView toolbarTitle;
+    private View filter;
+    private View cleanFilters;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.toolbar = findViewById(R.id.main_filter_Toolbar);
+        setSupportActionBar(toolbar);
+
+        this.toolbarTitle = findViewById(R.id.main_titleLabel);
+        this.filter = findViewById(R.id.main_filterButton);
+        this.cleanFilters = findViewById(R.id.main_cleanFilterButton);
 
         this.listOLFragment = new ListOLFragment();
         this.detailOLFragment = new DetailOLFragment();
@@ -29,5 +44,24 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.list_container, fragment).addToBackStack(fragment.getTag());
         fragmentTransaction.commit();
+    }
+
+    public void showFilterButton() {
+        this.cleanFilters.setVisibility(View.GONE);
+        this.filter.setVisibility(View.VISIBLE);
+    }
+
+    public void showCleanFiltersButton() {
+        this.cleanFilters.setVisibility(View.VISIBLE);
+        this.filter.setVisibility(View.GONE);
+    }
+
+    public void removeToolbarButtons() {
+        this.cleanFilters.setVisibility(View.GONE);
+        this.filter.setVisibility(View.GONE);
+    }
+
+    public void setToolbarMessage(String message) {
+        this.toolbarTitle.setText(message);
     }
 }
